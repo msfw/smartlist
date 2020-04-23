508c2faf1f8b8c8c3d9daed701ea08a1146c2112
Andrei Franz Salvador <andrei.salvador@lincros.com>
2020-04-22 22:32:29 -0300

    Border Radius

--- src/components/EditLabel.vue
+++ src/components/EditLabel.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container-edit-label" :class="{readonly: !editMode}">
-      <input class="input-border" v-if="editMode" type="text" v-focus @focusout="exitEdit" @keypress.enter="exitEdit" v-model="inputText"/>
+      <input class="input-border" v-if="editMode" type="text" v-focus @focusout="exitEdit" @keypress.enter="exitEdit" v-model="inputText" :placeholder="placeHolder"/>
       <span v-if="!editMode" @click="enterEdit">{{ inputText }}</span>
   </div>
 </template>
@@ -22,6 +22,10 @@ export default {
     showUntitled: {
       type: Boolean,
       default: false
+    },
+    placeHolder:{
+      type: String,
+      default: ''
     }
   },
   data() {
